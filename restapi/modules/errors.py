5dd29518734094c2e699b5acaab8135a701ed23f
Haukur Kristinsson <haukur@hauxi.is>
2014-07-11 00:47:46 +0000

    Errors refactored.

--- restapi/modules/errors.py
+++ restapi/modules/errors.py
@@ -2,12 +2,19 @@ __author__ = 'haukurk'
 
 
 def log_exception(sender, exception, **extra):
-    """ Log an exception to our logging framework """
+    """
+    Log an exception to our logging framework.
+    @param sender: sender
+    @param exception: exception triggered
+    @**extra: other params.
+    @return: void
+    """
     sender.logger.debug('Got exception during processing: %s', exception)
 
 
 def error_incorrect_version(version):
     """
+    Return a response when the client is using incorrect API version.
     @param version: version in use.
     @return: dict
     """
@@ -16,13 +23,16 @@ def error_incorrect_version(version):
 
 def error_object_not_found():
     """
-
+    Return an error response when something is not found, like a object in a database.
+    @return: dict
     """
     return {"status": "error", "message": "object not found"}
 
 
-def error_commit_error():
+def error_commit_error(ex):
     """
-
+    Return an error response when database commit fails somehow.
+    Like when inserting into a database and you get a unique constraint violated.
+    @return: dict
     """
-    return {"status": "error", "message": "error when committing object to database"}
+    return {"status": "error", "message": "error when committing object to database", "exception": ex.message}
